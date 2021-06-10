a2cb8a80f0f18baddbf6904cebc825289dc162ee
Vikas Ukani <vikas@maven.one>
2021-06-10 15:55:57 +0530

    WIP -

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// const withPWA = require('next-pwa')
+/* , withPWA({
+    pwa: { dest: 'public' }
+}) */
+module.exports = {
+    images: {
+        domains: ['dkrr9o6ir0yt2.cloudfront.net'],
+    }
+}
