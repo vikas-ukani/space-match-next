2723a4b071c2f2e16f632abcc7f43006969711db
Vikas Ukani <vikas@maven.one>
2021-06-25 18:30:23 +0530

    WIP: Declined Space Model Data Filling...

---
+++ Components/Common/Sliders/Tenant/DeclinedSpaces/DeclinedSpacesSlider.jsx
@@ -0,0 +1,93 @@
+import Slider from "react-slick";
+import { getSmallImageUrl } from "@/utils/image_path";
+
+
+const inner_arrow__css = {
+    height: "24px",
+    width: "24px",
+    borderRadius: "24px",
+    backgroundColor: "rgb(174 174 174 / 30%)",
+    display: "flex !important",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: "-50px",
+    transform: "translateY(-50%)",
+    zIndex: "1",
+    cursor: "pointer",
+}
+
+
+const PrevArrow = ({ className, style, onClick }) => (
+    <div className="" style={{
+        ...style,
+        ...inner_arrow__css,
+        top: "22vh",
+        height: "28px",
+        width: "28px",
+        backgroundColor: "rgba(255, 255, 255, .55)",
+        left: "18px",
+        paddingLeft: "10px",
+        paddingTop: "2px",
+    }}
+        onClick={onClick}
+    >
+        <i className={"icon icon-small-chevron-right "}  ></i>
+    </div>
+)
+
+const NextArrow = ({ className, style, onClick }) => (
+    <div className="" style={{
+        ...style,
+        top: "20vh",
+        justifyContent: "center",
+        position: "absolute",
+        borderRadius: "28px",
+        cursor: "pointer",
+        height: "28px",
+        width: "28px",
+        backgroundColor: "rgba(255, 255, 255, .55)",
+        right: "20px",
+        paddingLeft: "10px",
+        paddingTop: "2px",
+    }}
+        onClick={onClick}
+    >
+        <i className={"icon icon-small-chevron-left "}></i>
+    </div >
+)
+
+
+
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    adaptiveHeight: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: <PrevArrow />,
+    // nextArrow: <NextArrow />,
+
+}
+
+const DeclinedSpacesSlider = ({ images, setting = {} }) => {
+    const properties = { ...settings, ...setting }
+    // <div class="space-image-item"><img src="'+new_image_path+'" class="img-fluid" alt="'+property_images[pi]['property_image_name']+'"/></div>
+    return (
+        <Slider {...properties}>
+            {images?.map(spaceImage => {
+                return (
+                    <div key={spaceImage.id} className="space-image-item">
+                        <img src={getSmallImageUrl(spaceImage.property_image_path)}
+                            className="img-fluid  "
+                            alt={spaceImage.property_image_name} />
+                    </div>
+                )
+            })}
+        </Slider>
+    );
+}
+
+export default DeclinedSpacesSlider;
---
+++ Components/Tenant/RentalManagement/Modals/DeclinedSpacesModal.jsx
@@ -0,0 +1,116 @@
+import DeclinedSpacesSlider from "@/Components/Common/Sliders/Tenant/DeclinedSpaces/DeclinedSpacesSlider";
+import FavoriteImageSlider from "@/Components/Common/Sliders/Tenant/Favorite/FavoriteImageSlider";
+import { Modal } from "react-bootstrap";
+
+const DeclinedSpacesModal = ({ show, setShow, data }) => {
+    console.log("Modal Data", data);
+
+    return (
+        <Modal
+            show={show}
+            size={'xl'} backdrop="static"
+            onHide={() => setShow(false)}
+            dialogClassName="modal-90w"
+            aria-labelledby="example-custom-modal-styling-title"
+        >
+            <Modal.Header closeButton>
+                <p className="text-dark-grey" >
+                    {data?.created_date}
+                </p>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="row">
+                    <div className="mb-5 col-lg-12 space-item ">
+                        <div className=" d-flex">
+                            {/* <DeclinedSpacesSlider images={data?.property?.spacePropertyImages} /> */}
+                            {/* <FavoriteImageSlider setting={{ dots: true }} favoriteImages={data?.property?.spacePropertyImages} /> */}
+                            {/* <SpaceImageSlider spaceDetail={list} /> */}
+                        </div>
+                        {/* <pre>{JSON.stringify(data?.property.spacePropertyImages, null, 1)}</pre> */}
+                    </div>
+                    {/* <div className="col-lg-12">
+                        <div className="space-item mb-4">
+                            <DeclinedSpacesSlider images={data.property.spacePropertyImages} />
+                        </div>
+                    </div> */}
+                    <div className="col-lg-12">
+                        <div className="list-space-card list-rental-card d-lg-flex align-items-lg-center">
+                            <div className="form-group">
+                                <div className="space-item">
+                                    <a className="space-title text-truncate mt-0" target="_blank" id="enquiry_property_name"></a>
+                                    <div>
+                                        <p className="space-address text-truncate" id="enquiry_address"></p>
+                                        <p className="space-size" id="enquiry_property_size"></p>
+                                    </div>
+                                    <div>
+                                        <p className="space-price">R <span id="enquiry_price"></span> <span className="space-size">for <span id="enquiry_days"></span> days</span></p>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="form-group">
+                                <div className="list-space-date d-flex justify-content-end flex-lg-row flex-column">
+                                    <p className="btn font-weight-medium bg-light-grey mb-2 cursor-unset">
+                                        <span className="text-dark-grey text-capitalize">Start:</span> <span id="enquiry_start_date"></span>
+                                    </p>
+                                    <p className="btn font-weight-medium bg-light-grey mb-2 ml-lg-3 ml-0 mt-0 cursor-unset">
+                                        <span className="text-dark-grey text-capitalize">End:</span> <span id="enquiry_end_date"></span>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-12">
+                        <div className="row">
+                            <div className="col-lg-6">
+                                <div className="detail-information mb-4">
+                                    <p className="detail-perday mb-3">Entity Details</p>
+                                    <p id="entity_individually">Booking is done individually.Not booked on behalf of entity.</p>
+                                    <ul className="text-grey" id="entity_behalf">
+                                        <li><span>Entity Name:</span> <span id="entity_name"></span></li>
+                                        <li><span>Vat Number:</span> <span id="entity_vat"></span></li>
+                                        <li id="website_detail"><span>Website:</span> <span id="website_url">www.ooberlojetco.co.za</span></li>
+                                    </ul>
+                                </div>
+
+                                <div className="detail-information mb-4">
+                                    <p className="detail-perday mb-3">What will you use this space for?</p>
+
+                                    <div className="detail-share mb-0 detail-share-space" id="space_used_for">
+                                    </div>
+                                </div>
+
+                                <div className="detail-information mb-4">
+                                    <p className="detail-perday mb-3">More about your Space</p>
+
+                                    <ul className="text-grey">
+                                        <li className="d-md-flex"><p className="mr-md-4">Name of your project/application:</p> <p className="
+											font-weight-normal" id="project_name"></p></li>
+                                    </ul>
+
+                                    <p className="text-grey mt-3" id="project_desc"></p>
+                                </div>
+                            </div>
+
+                            <div className="col-lg-6">
+                                <div className="detail-information mb-4">
+                                    <p className="detail-perday mb-3">What will the space look like?</p>
+
+                                    <p className="text-grey mt-3" id="space_look_like"></p>
+                                </div>
+
+                                <div className="detail-information mb-4">
+                                    <ul className="form-row detail-image" id="enquiry_iamges">
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Modal.Body>
+        </Modal>
+
+    );
+}
+
+export default DeclinedSpacesModal;
