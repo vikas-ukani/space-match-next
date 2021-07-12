6ca66294304e171379c12eb1ef28a208331e040e
Vikas Ukani <vikas@maven.one>
2021-07-12 18:32:35 +0530

    WIP: Bank Details API Service Implementing...

--- pages/tenant-bank-detail/index.jsx
+++ pages/tenant-bank-detail/index.jsx
@@ -1,6 +1,25 @@
-const index = () => {
+import DashboardSideNav from "@/Layouts/Dashboard/DashboardSideNav";
+import { axiosCall } from "@/lib/useSWRAxios";
+
+
+export async function getServerSideProps({ req}) {
+    const { token_type, token } = req.cookies
+    
+    console.log('asd', token_type);
+
+    return {
+        props: {
+            data: ""
+        }
+    }
+}
+
+
+
+const TenantBankDetail = () => {
     return (
         <div>
+            <DashboardSideNav />
             <div className="content-wrapper">
                 <div className="container-fluid">
                     <div className="row">
@@ -18,7 +37,7 @@ const index = () => {
         }
                         @endphp */}
                         <div className="col-lg-12">
-                            <form className="form-dashboard" id="bankLandlordForm" name="bankLandlordForm" method="POST" enctype="multipart/form-data" autocomplete="off">
+                            <form className="form-dashboard" id="bankLandlordForm" name="bankLandlordForm" method="POST" encType="multipart/form-data" autoComplete="off">
                                 <div className="row">
                                     <div className="col-xl-4">
                                         <div className="section-title mb-3">
@@ -34,21 +53,27 @@ const index = () => {
                                         <div className="form-group">
                                             <label>Bank</label>
                                             <input type="text" className="form-control" id="bankname" name="bankname"
-                                                value="{{ !empty($bankDetails->bankname) ? $bankDetails->bankname : ''  }}" @if($hidden) readonly @endif />
+                                                defaultValue="{{ !empty($bankDetails->bankname) ? $bankDetails->bankname : ''  }}"
+                                            // @if($hidden) readonly @endif 
+                                            />
                                             <em id="bankname_error" className="error invalid-feedback"></em>
                                         </div>
 
                                         <div className="form-group">
                                             <label>Name of Account Holder</label>
                                             <input type="text" className="form-control" id="nameaccountholder" name="nameaccountholder"
-                                                value="{{ !empty($bankDetails->nameaccountholder) ? $bankDetails->nameaccountholder : ''  }}" @if($hidden) readonly @endif />
+                                                defaultValue="{{ !empty($bankDetails->nameaccountholder) ? $bankDetails->nameaccountholder : ''  }}"
+                                            // @if($hidden) readonly @endif
+                                            />
                                             <em id="nameaccountholder_error" className="error invalid-feedback"></em>
                                         </div>
 
                                         <div className="form-group">
                                             <label>Account Number</label>
                                             <input type="text" className="form-control" id="accountnumber" name="accountnumber"
-                                                value="{{ !empty($bankDetails->accountnumber) ? $bankDetails->accountnumber : ''  }}" @if($hidden) readonly @endif />
+                                                defaultValue="{{ !empty($bankDetails->accountnumber) ? $bankDetails->accountnumber : ''  }}"
+                                            // @if($hidden) readonly @endif 
+                                            />
                                             <em id="accountnumber_error" className="error invalid-feedback"></em>
                                         </div>
 
@@ -57,12 +82,12 @@ const index = () => {
                                             <select className="select2" id="accounttype" name="accounttype"
                                             // @if($hidden) readonly @endif 
                                             >
-                                                <option value="saving"
+                                                <option defaultValue="saving"
                                                 // {{ (!empty($bankDetails -> accounttype) && $bankDetails -> accounttype == 'saving') ? 'selected' : ''}}
                                                 >
                                                     Saving
                                                 </option>
-                                                <option value="current"
+                                                <option defaultValue="current"
                                                 // {{ (!empty($bankDetails -> accounttype) && $bankDetails -> accounttype == 'current') ? 'selected' : ''}} 
                                                 >Current</option>
                                             </select>
@@ -71,7 +96,7 @@ const index = () => {
                                         <div className="form-group">
                                             <label>Branch Code</label>
                                             <input type="text" className="form-control" id="branchcode" name="branchcode"
-                                                value="{{ !empty($bankDetails->branchcode) ? $bankDetails->branchcode : ''  }}"
+                                                defaultValue="{{ !empty($bankDetails->branchcode) ? $bankDetails->branchcode : ''  }}"
                                             // @if($hidden) readonly @endif 
                                             />
                                             <em id="branchcode_error" className="error invalid-feedback"></em>
@@ -89,7 +114,7 @@ const index = () => {
                                                 <input type="file" className="custom-file-input" id="bankdocument" name="bankdocument"
                                                 // @if($hidden) disabled @endif 
                                                 />
-                                                <label className="custom-file-label font-weight-bold" for="bankdocument">Upload</label>
+                                                <label className="custom-file-label font-weight-bold" htmlFor="bankdocument">Upload</label>
                                                 {/* {{-- < span className="text-dark-grey"> </> --}} */}
                                                 {/* @php
                   if( !empty($bankDetails->documents) )
@@ -123,21 +148,21 @@ const index = () => {
                                             {/* // { */}
                                             {/* // if($bankDetails->documents[0]->document_status == 1) {  */}
                                             {/* @endphp */}
-                                            <a className="btn btn-rounded btn-success btn-block" id="btn-verified" style="color:white;">Verified</a>
-                                            <a className="btn btn-rounded btn-warning btn-block" id="btn-pending-approval" style="color:white;display: none;">Pending Approval</a>
+                                            {/* <a className="btn btn-rounded btn-success btn-block" id="btn-verified" style={{ color: "white" }}>Verified</a> */}
+                                            {/* <a className="btn btn-rounded btn-warning btn-block" id="btn-pending-approval" style={{ color: "white", display: "none" }}>Pending Approval</a> */}
                                             {/* // @php */}
                                             {/* // } */}
                                             {/* // if($bankDetails->documents[0]->document_status == 0) { */}
                                             {/* @endphp */}
-                                            <a className="btn btn-rounded btn-warning btn-block" id="btn-pending-approval" style="color:white;">Pending Approval</a>
-                                            <a className="btn btn-rounded btn-success btn-block" id="btn-verified" style="display: none;color:white;">Verified</a>
+                                            {/* <a className="btn btn-rounded btn-warning btn-block" id="btn-pending-approval" style={{ color: "white" }}>Pending Approval</a> */}
+                                            {/* <a className="btn btn-rounded btn-success btn-block" id="btn-verified" style={{ display: 'none', color: "white" }}>Verified</a> */}
                                             {/* // @php */}
                                             {/* // } */}
                                             {/* // } */}
                                             {/* // else { */}
                                             {/* //    @endphp */}
-                                            <a className="btn btn-rounded btn-success btn-block" id="btn-verified" style="color:white;">Verified</a>
-                                            <a className="btn btn-rounded btn-warning btn-block" id="btn-pending-approval" style="color:white;">Pending Approval</a>
+                                            {/* <a className="btn btn-rounded btn-success btn-block" id="btn-verified" style={{ color: "white" }}>Verified</a> */}
+                                            {/* <a className="btn btn-rounded btn-warning btn-block" id="btn-pending-approval" style={{ color: "white" }}>Pending Approval</a> */}
                                             {/* // @php */}
                                             {/* // } */}
                                             {/* // @endphp */}
@@ -148,8 +173,8 @@ const index = () => {
                                 <div className="row border-top pt-4">
                                     <div className="col-xl-4 ml-auto">
                                         <div className="form-group">
-                                            <input type="hidden" name="bank_id" id="bank_id" value="{{ !empty($bankDetails->id) ? $bankDetails->id : ''  }}" />
-                                            <input type="hidden" name="action_name" id="action_name" value="{{ $is_add_update }}" />
+                                            <input type="hidden" name="bank_id" id="bank_id" defaultValue="{{ !empty($bankDetails->id) ? $bankDetails->id : ''  }}" />
+                                            <input type="hidden" name="action_name" id="action_name" defaultValue="{{ $is_add_update }}" />
                                             {/* @if(!$hidden) */}
                                             <button type="button" id="updateDocument" className="btn btn-primary btn-block">Add/Update</button>
                                             {/* @endif */}
@@ -165,4 +190,4 @@ const index = () => {
     );
 }
 
-export default index;
+export default TenantBankDetail;
